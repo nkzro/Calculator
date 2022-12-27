dc6419546ec5b05a818838d8317e02a2cf8c9bcc
Samuel Adu <samuelnkadu@gmail.com>
2022-12-26 21:13:15 -0500

    Buttons arranged in middle of calculator;

--- calculator.js
+++ calculator.js
@@ -1,89 +1,89 @@
-const display = document.querySelector('.display');
-const numbers = document.querySelectorAll('.button.number');
-const deleteBtn = document.querySelector('.button#delete');
-const clearBtn = document.querySelector('.button#clear');
-const operators = document.querySelectorAll('.button.operator');
-const equals = document.querySelector('.button.evaluate');
+// const display = document.querySelector('.display');
+// const numbers = document.querySelectorAll('.button.number');
+// const deleteBtn = document.querySelector('.button#delete');
+// const clearBtn = document.querySelector('.button#clear');
+// const operators = document.querySelectorAll('.button.operator');
+// const equals = document.querySelector('.button.evaluate');
 
-let firstNumber = '';
-let secondNumber = '';
-let operator = '';
-let result = '';
+// let firstNumber = '';
+// let secondNumber = '';
+// let operator = '';
+// let result = '';
 
-let isOperatorSet = false;
-
-
-//Operations needed for basic arithmetic.
-function add(firstNumber, secondNumber) {
-    return firstNumber + secondNumber;
-}
-
-function subtract(firstNumber, secondNumber) {
-    return firstNumber - secondNumber;
-}
-
-function multiply(firstNumber, secondNumber) {
-    return firstNumber * secondNumber;
-}
-
-function divide(firstNumber, secondNumber) {
-    if (secondNumber === 0) {
-        return "Undefined";
-    }
-    return firstNumber/secondNumber;
-}
-
-function operate (operator, firstNumber, secondNumber) {
-    let operation;
-    switch (operator) {
-        case '+':
-            operation = add(firstNumber, secondNumber);
-            break;
-        case '-':
-            operation = subtract(firstNumber, secondNumber);
-            break;
-        case 'x':
-            operation = multiply(firstNumber, secondNumber);
-            break;
-        case '÷':
-            operation = divide(firstNumber, secondNumber);
-            break;
-    }
-    return operation;
-}
+// let isOperatorSet = false;
 
 
-/* Event listeners for buttons */
+// //Operations needed for basic arithmetic.
+// function add(firstNumber, secondNumber) {
+//     return firstNumber + secondNumber;
+// }
 
-function setOperator(sign) {
-    return operator;
-}
+// function subtract(firstNumber, secondNumber) {
+//     return firstNumber - secondNumber;
+// }
 
-numbers.forEach(number => {
-    number.addEventListener('click', () => {
-        display.textContent += number.textContent;
-        firstNumber += number.textContent;
-        if (number.classList.contains('operator')) {
-            setOperator(number.textContent);
-            console.log(operator)
-            //secondNumber += number.textContent;
-        }
-        else if (number.classList.contains('evaluate')){
-            operate(operator, firstNumber, secondNumber);
-        }
-    })
-});
+// function multiply(firstNumber, secondNumber) {
+//     return firstNumber * secondNumber;
+// }
 
-function evaluate() {
-    return operate(operator, firstNumber, secondNumber);
-}
+// function divide(firstNumber, secondNumber) {
+//     if (secondNumber === 0) {
+//         return "Undefined";
+//     }
+//     return firstNumber/secondNumber;
+// }
 
-//Deletes the last element when you click the delete button
-deleteBtn.addEventListener('click', () => {
-    display.textContent = display.textContent.slice(0, -1);
-})
+// function operate (operator, firstNumber, secondNumber) {
+//     let operation;
+//     switch (operator) {
+//         case '+':
+//             operation = add(firstNumber, secondNumber);
+//             break;
+//         case '-':
+//             operation = subtract(firstNumber, secondNumber);
+//             break;
+//         case 'x':
+//             operation = multiply(firstNumber, secondNumber);
+//             break;
+//         case '÷':
+//             operation = divide(firstNumber, secondNumber);
+//             break;
+//     }
+//     return operation;
+// }
 
-//Clears the display, leaving it blank.
-clearBtn.addEventListener('click', () => {
-    display.textContent = null;
-})
+
+// /* Event listeners for buttons */
+
+// function setOperator(sign) {
+//     return operator;
+// }
+
+// numbers.forEach(number => {
+//     number.addEventListener('click', () => {
+//         display.textContent += number.textContent;
+//         firstNumber += number.textContent;
+//         if (number.classList.contains('operator')) {
+//             setOperator(number.textContent);
+//             console.log(operator)
+//             //secondNumber += number.textContent;
+//         }
+//         else if (number.classList.contains('evaluate')){
+//             operate(operator, firstNumber, secondNumber);
+//         }
+//     })
+// });
+
+// function evaluate() {
+//     return operate(operator, firstNumber, secondNumber);
+// }
+
+// //Deletes the last element when you click the delete button
+// deleteBtn.addEventListener('click', () => {
+//     display.textContent = display.textContent.slice(0, -1);
+// })
+
+// //Clears the display, leaving it blank.
+// clearBtn.addEventListener('click', () => {
+//     display.textContent = null;
+// })
